5a9c4989352c1b9593751bc6eda13ec5bfd313b5
ThaisKurmann <thais.martin.ferraz@gmail.com>
2022-03-23 15:16:23 +0100

    Segunda versao programa IMC Orientado a Objetos

---
+++ Atividade7/Pessoa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade7
+{
+    class Pessoa
+    {
+
+         public double peso;
+         public double altura;
+
+
+
+        public Pessoa(double altura, double peso)
+        {
+            this.peso = peso;
+            this.altura = altura;
+
+        }
+
+        public double IMC()
+        {
+            return peso / (altura * altura);
+        }
+
+
+
+
+        public string ResultIMC()
+        {
+
+            if (IMC() < 18.5)
+            {
+                //MAGREZA
+                return "Atencao! Estado de magreza. IMC = " + IMC().ToString("F0", CultureInfo.InvariantCulture);
+
+            }
+            else if (IMC() > 18.5 && IMC() < 24.9)
+            {
+                //NORMAL
+                return "Estado normal. IMC = " + IMC().ToString("F0", CultureInfo.InvariantCulture);
+            }
+            else if (IMC() > 24.9 && IMC() < 30)
+            {
+                //SOBREPESO
+                return "Cuidado. Estado de sobrepeso. IMC = " + IMC().ToString("F0", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                //OBESIDADE
+                return "Atencao! Estado de Obesidade. IMC = " + IMC().ToString("F0", CultureInfo.InvariantCulture);
+            }
+
+        }
+    }
+}
--- Atividade7/Program.cs
+++ Atividade7/Program.cs
@@ -7,41 +7,22 @@ namespace Atividade7
     {
         static void Main(string[] args)
         {
+
+
             //Formula: IMC = peso / (altura * altura)
-            Console.WriteLine("Calculo de IMC");
-            Console.WriteLine("Entre com seu peso : ");
-            double peso;
-            peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+             Console.WriteLine("Calculo de IMC");
+             Console.WriteLine("Entre com seu peso : ");
+             double peso;
+             peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Entre com sua altura em metros: ");
-            double altura;
-            altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double IMC = (peso/(altura * altura));
-            
-            
-            if (IMC < 18.5) {
-                //MAGREZA
-                Console.WriteLine("Atencao! Estado de magreza. IMC = " + IMC.ToString("F0", CultureInfo.InvariantCulture));
-                
-            }else if ( IMC > 18.5 && IMC < 24.9)
-            {
-                //NORMAL
-                Console.WriteLine("Estado normal. IMC = " + IMC.ToString("F0", CultureInfo.InvariantCulture));      
-            }else if (IMC > 24.9 && IMC < 30)
-            {
-                //SOBREPESO
-                Console.WriteLine("Cuidado. Estado de sobrepeso. IMC = " + IMC.ToString("F0", CultureInfo.InvariantCulture));
-            }
-            else
-            {
-                //OBESIDADE
-                Console.WriteLine("Atencao! Estado de Obesidade. IMC = " + IMC.ToString("F0", CultureInfo.InvariantCulture));
-            }
-
-            
+             Console.WriteLine("Entre com sua altura em metros: ");
+             double altura;
+             altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
 
+            Pessoa pessoa = new Pessoa(altura, peso);
+
+            Console.WriteLine(pessoa.ResultIMC());
 
         }
     }
